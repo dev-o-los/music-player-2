f08f86915e53084ae6afb8167953e465864eb466
devolos <utk27032005@gmail.com>
2025-01-19 08:57:38 +0530

    Remove unused settings and terms.html; update music paths in script.js for consistency

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,58 +6,58 @@
 
 const musicData = [
   {
-    backgroundImage: "./assets/images/poster-1.jpg",
-    posterUrl: "./assets/images/poster-1.jpg",
+    backgroundImage: "/assets/images/poster-1.jpg",
+    posterUrl: "/assets/images/poster-1.jpg",
     title: "Happy Moments (Master)",
     album: "No Spirit",
     year: 2022,
     artist: "No Spirit x Tonion",
-    musicPath: "./assets/music/music-1.mp3",
+    musicPath: "/assets/music/music-1.mp3",
   },
   {
-    backgroundImage: "./assets/images/poster-2.jpg",
-    posterUrl: "./assets/images/poster-2.jpg",
+    backgroundImage: "/assets/images/poster-2.jpg",
+    posterUrl: "/assets/images/poster-2.jpg",
     title: "We Are Going To Be Ok (Master)",
     album: "No Spirit",
     year: 2022,
     artist: "No Spirit x Jhove",
-    musicPath: "./assets/music/music-2.mp3",
+    musicPath: "/assets/music/music-2.mp3",
   },
   {
-    backgroundImage: "./assets/images/poster-3.jpg",
-    posterUrl: "./assets/images/poster-3.jpg",
+    backgroundImage: "/assets/images/poster-3.jpg",
+    posterUrl: "/assets/images/poster-3.jpg",
     title: "Winter Meadow",
     album: "No Spirit",
     year: 2022,
     artist: "No Spirit x  juniorodeo",
-    musicPath: "./assets/music/music-3.mp3",
+    musicPath: "/assets/music/music-3.mp3",
   },
   {
-    backgroundImage: "./assets/images/poster-4.jpg",
-    posterUrl: "./assets/images/poster-4.jpg",
+    backgroundImage: "/assets/images/poster-4.jpg",
+    posterUrl: "/assets/images/poster-4.jpg",
     title: "From Where We Started",
     album: "No Spirit",
     year: 2022,
     artist: "No Spirit",
-    musicPath: "./assets/music/music-4.mp3",
+    musicPath: "/assets/music/music-4.mp3",
   },
   {
-    backgroundImage: "./assets/images/poster-5.jpg",
-    posterUrl: "./assets/images/poster-5.jpg",
+    backgroundImage: "/assets/images/poster-5.jpg",
+    posterUrl: "/assets/images/poster-5.jpg",
     title: "Where I Found You",
     album: "No Spirit",
     year: 2022,
     artist: "No Spirit",
-    musicPath: "./assets/music/music-5.mp3",
+    musicPath: "/assets/music/music-5.mp3",
   },
   {
-    backgroundImage: "./assets/images/zayn.jpg",
-    posterUrl: "./assets/images/zayn.jpg",
+    backgroundImage: "/assets/images/zayn.jpg",
+    posterUrl: "/assets/images/zayn.jpg",
     title: "Better",
     album: "ZAYN",
     year: 2022,
     artist: "ZAYN",
-    musicPath: "./assets/music/music-6.mp3",
+    musicPath: "/assets/music/music-6.mp3",
   },
 ];
 
@@ -351,6 +351,24 @@ const repeat = function () {
     this.classList.remove("active");
   }
 };
+
+//playing selected music
+
+function getQueryParam(param) {
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get(param);
+}
+
+function selectMusic() {
+  const val = getQueryParam("value");
+  currentMusic = +val;
+  changePlayerInfo();
+  audioSource.play();
+  playBtn.classList.add("active");
+  playInterval = setInterval(updateRunningTime, 500);
+}
+
+selectMusic();
 
 playerRepeatBtn.addEventListener("click", repeat);
 
